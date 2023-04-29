1930c62fb81a661744f598f641188f7bedcb5ba3
Zak Forster <zak.forster16@gmail.com>
2023-04-29 22:11:18 +0100

    add copy link

--- src/components/recommendation/container.tsx
+++ src/components/recommendation/container.tsx
@@ -1,9 +1,19 @@
-import { Card, Center, Text, Divider } from "@mantine/core";
+import {
+  Card,
+  Center,
+  Text,
+  Divider,
+  Menu,
+  Button,
+  ActionIcon,
+  Container,
+} from "@mantine/core";
 import { Carousel } from "@mantine/carousel";
 import { useState } from "react";
 import { RecommendationResponse } from "../../datatypes/recommendation";
 import Book from "./book";
 import { DateTime } from "luxon";
+import { IconCopy, IconDotsVertical, IconMenu2 } from "@tabler/icons";
 
 type BookContainerProps = {
   recommendationResponse: RecommendationResponse;
@@ -11,16 +21,41 @@ type BookContainerProps = {
 
 const BookContainer = ({ recommendationResponse }: BookContainerProps) => {
   const [resetCollapse, setResetCollapse] = useState(false);
+  const [openMenu, setOpenMenu] = useState(false);
 
   const date = new Date(recommendationResponse?.timestamp);
   const localTimestamp = new Date(
     date.getTime() - date.getTimezoneOffset() * 60000
   ).toISOString();
 
+  const handleCopyId = (id: string) => {
+    navigator.clipboard.writeText(id);
+  };
+
   return (
     <Center p="xl">
       <Card p="xl" withBorder>
         <Card.Section p="xs">
+          <div
+            style={{
+              position: "absolute",
+              top: "10px",
+              right: "10px",
+            }}
+          >
+            <Menu opened={openMenu} onChange={setOpenMenu}>
+              <Menu.Target>
+                <ActionIcon size="sm">
+                  <IconDotsVertical />
+                </ActionIcon>
+              </Menu.Target>
+
+              <Menu.Dropdown>
+                <Menu.Item icon={<IconCopy size={14} />}>Copy Link</Menu.Item>
+              </Menu.Dropdown>
+            </Menu>
+          </div>
+
           <Text size={"xs"} align="center">
             {`Posted ${DateTime.fromISO(localTimestamp).toRelative()}`}
           </Text>
