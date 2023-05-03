c11b9f41c0208aad8ba6e980a4d3948bcea4dbfb
Zak Forster <zak.forster16@gmail.com>
2023-05-03 23:40:58 +0100

    fiz

--- src/index.tsx
+++ src/index.tsx
@@ -26,7 +26,7 @@ root.render(
             }
           />
           <Route
-            path="/recommendation/:id"
+            path="recommendation/:id"
             element={
               <App>
                 <RecommendationById />
