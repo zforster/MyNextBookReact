ba123a61cef27d176e1b1f0adedc2343b6747aae
Zak Forster <zak.forster16@gmail.com>
2023-06-11 22:14:43 +0100

    add animated nutton

--- src/containers/landing.tsx
+++ src/containers/landing.tsx
@@ -1,5 +1,5 @@
 import {
-  AppShell,
+  Button,
   Center,
   Container,
   MantineProvider,
@@ -7,53 +7,70 @@ import {
   Title,
 } from "@mantine/core";
 import { Helmet } from "react-helmet-async";
+import "../styles/animation.css";
 
 const Landing = () => {
-  const mainTitle = () => (
-    <Center style={{ display: "flex", flexDirection: "column" }}>
-      <Title pt="xl" size="h2" color="#16b576">
-        PagePundit
-      </Title>
-
-      <Container
-        style={{
-          display: "flex",
-          justifyContent: "center",
-          alignContent: "center",
-        }}
-      >
-        <Title size="40px" pt="lg" align="center">
-          AI Book Recommendations
-          <span
-            style={{
-              paddingLeft: "10px",
-              fontSize: "40px",
-            }}
-          >
-            📖
-          </span>
-        </Title>
-      </Container>
-
-      <Text maw="500px" align="center" size="20px" pt="lg" color="#71717a">
-        Let{" "}
-        <span style={{ color: "#2e2e2e", fontWeight: "bold" }}>
-          Artifical Intelligence
-        </span>{" "}
-        take the hassle out of finding your next great read
-      </Text>
-    </Center>
-  );
-
   return (
     <MantineProvider withGlobalStyles withNormalizeCSS>
-      <AppShell>
-        <Helmet>
-          <title>PagePundit</title>
-          <link rel="canonical" href={`${window.location}`} />
-        </Helmet>
-        {mainTitle()}
-      </AppShell>
+      <Helmet>
+        <title>PagePundit</title>
+        <link rel="canonical" href={`${window.location}`} />
+      </Helmet>
+      <Center
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          paddingTop: "50px",
+        }}
+      >
+        <Title py="xl" size="h2" color="#16b576">
+          PagePundit
+        </Title>
+
+        <Container
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            alignContent: "center",
+            textShadow: "0 3px 0 #e2e2e2",
+          }}
+        >
+          <Title size="50px" align="center">
+            AI Book Recommendations
+            <span
+              style={{
+                paddingLeft: "10px",
+                fontSize: "50px",
+              }}
+            >
+              📖
+            </span>
+          </Title>
+        </Container>
+
+        <Text maw="500px" align="center" size="20px" py="xl" color="#71717a">
+          Let{" "}
+          <span style={{ color: "#2e2e2e", fontWeight: "bold" }}>
+            Artifical Intelligence
+          </span>{" "}
+          take the hassle out of finding your next great read
+        </Text>
+
+        <Container py="xl">
+          <Button
+            className="gradient-button"
+            style={{
+              background:
+                "linear-gradient(60deg, #16b576, #5073b8, #1098ad, #07b39b, #6fba82)",
+              animation: "gradientAnimation 3.5s ease infinite alternate",
+              backgroundSize: "300% 300%",
+            }}
+            size="md"
+          >
+            Get Started Now!
+          </Button>
+        </Container>
+      </Center>
     </MantineProvider>
   );
 };
